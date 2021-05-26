ea6815f4e88eeaffd6eb48a3fed54d6ea7c6e490
Huzaifa <57568128+huzaifa06@users.noreply.github.com>
2021-05-26 09:57:39 +0530

    Delete WikiSearch-master/GUI directory

--- WikiSearch-master/GUI/index.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python indexing/start.py "$1" "$2"
--- WikiSearch-master/GUI/search/stemmer.py
+++
@@ -1,5 +0,0 @@
-
-class stemmer ():
-        def __init__ (self):
-            from stemming.porter2 import stem
-            self.stem = stem
